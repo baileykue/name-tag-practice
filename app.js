8fd9a4e910992c3773f2ad6d6922f0c62e102f98
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-05-26 16:46:14 -0700

    we made a nametag, yay

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // import functions
 
 // grab DOM elements
+const nameField = document.getElementById('name-field');
+const middle = document.getElementById('middle');
 
-// set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+nameField.addEventListener('input', (e) => {
+  middle.textContent = e.target.value;
+});
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
